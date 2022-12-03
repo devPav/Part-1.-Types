c80061557f0a4b91f3532058a57591631754de74
Pavel V <pash.in.out@gmail.com>
2022-12-03 22:39:12 +0300

    Initial Commit

---
+++ Part 1 Basics.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "See you"
+var age = 21, newAge = 22
+age = 22
+newAge
+
+let name: String
+name = "Peter Turbo"
+
+let myAge = 36
+var yourAge = myAge
+yourAge
