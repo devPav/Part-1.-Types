b526042a43ce3f179ea66b3eb8fba2399d496dba
Pavel V <pash.in.out@gmail.com>
2022-12-04 23:29:40 +0300

    Simple types

--- Part 1 Basics.playground/Contents.swift
+++ Part 1 Basics.playground/Contents.swift
@@ -1,14 +1,87 @@
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
-str = "See you"
-var age = 21, newAge = 22
-age = 22
-newAge
+// Numbers
+let numOne = 19
+let numTwo = 4
 
-let name: String
-name = "Peter Turbo"
+let numThree = 3.13
+let numFour = 1.1
 
-let myAge = 36
-var yourAge = myAge
-yourAge
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let qo = numOne / numTwo
+numOne % numTwo
+numOne.isMultiple(of: numTwo)
+
+let sumD = numThree + numFour
+let diffD = numThree - numFour
+let multD = numThree * numFour
+let qoD = numThree / numFour
+numThree.truncatingRemainder(dividingBy: numFour)
+
+let resD = Double(numOne) * numThree
+let resI = numOne * Int(numThree)
+
+var count = 19
+count += 5
+count *= 3
+count -= 3
+count %= 8
+
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let octalInteger = 0o21
+let hexIneger = 0x11
+
+var myWallet: Decimal = 0
+let incomeAfterOperation: Decimal = 0.1
+myWallet += incomeAfterOperation
+print(myWallet)
+myWallet += incomeAfterOperation
+print(myWallet)
+myWallet += incomeAfterOperation
+print(myWallet)
+
+// String and Chracter
+let char: Character = "a"
+let stringOne = "Dragon"
+var emptyString = ""
+var anotherEmptyString = String()
+// Error:
+//var emptyChar: Character = ""
+
+let longString = """
+    This is multy
+    raw string
+    """
+print(longString)
+
+let notEmptyString = String("Hello, world!")
+let someDoubleNumber = 74.22
+let someStringNumber = String(someDoubleNumber)
+
+let firstString = "Swift"
+let secondString = "Objective-C"
+let anotherString = "Swift"
+firstString == secondString
+firstString == anotherString
+
+let myCharOverUnicode: Character = "\u{041A}"
+let stringOverUnicode = "\u{41c}\u{438}\u{440}\u{20}\u{412}\u{430}\u{43C}\u{21}"
+
+let isDragon = true
+let isKnight: Bool = false
+
+typealias AgeType = UInt8
+var myAge: AgeType = 36
+
+type(of: myAge)
+//116
+
+
+
+
+
+
